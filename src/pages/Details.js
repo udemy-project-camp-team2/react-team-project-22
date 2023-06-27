4c0513c6f2e7b8dd7952edfea91d3ab26707e9a8
junseokoh-hub <lunulatecat@naver.com>
2023-06-27 09:31:29 +0900

    Chores: delete logs Docs: Readme

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,42 +1,38 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import styles from '../styles/detail.module.css'
-import {Link} from 'react-router-dom'
+import styles from "../styles/detail.module.css";
+import { Link } from "react-router-dom";
 
-const MovieDetalis = ({id,title,src}) => {
-    const [detail,setDetail] = useState('');
-    const {movieid} = useParams();
-    const url = `https://api.themoviedb.org/3/movie/${movieid}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+const MovieDetalis = () => {
+  const [detail, setDetail] = useState("");
+  const { movieid } = useParams();
+  const url = `https://api.themoviedb.org/3/movie/${movieid}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`;
 
-    console.log(movieid);
+  useEffect(() => {
+    async function movie_detail() {
+      const fetchUrl = await fetch(url);
+      const detailInfo = await fetchUrl.json();
 
-    useEffect(()=>{
-        async function movie_detail () {
-            const fetchUrl = await fetch(url);
-            const detailInfo = await fetchUrl.json();
+      setDetail(detailInfo);
+    }
 
-            setDetail(detailInfo)
-        }
-        
     movie_detail();
-    },[])
-    console.log(detail);
+  }, []);
 
-    return (
-        <div className={styles.detail_page}>
-            <span className={styles.back}>
-                <Link to='/'>
-                    &lt; Back More
-                </Link>
-            </span>
-            <h2>
-                {detail?.title}
-            </h2>
-            <img src={`https://image.tmdb.org/t/p/original${detail?.poster_path}`} alt={detail?.title} />
-            
-            <p className={styles.overview}>{detail?.overview}</p>
-            <p>release date : {detail?.release_date}</p>
-        </div>
-    )
-}
+  return (
+    <div className={styles.detail_page}>
+      <span className={styles.back}>
+        <Link to="/">&lt; Back More</Link>
+      </span>
+      <h2>{detail?.title}</h2>
+      <img
+        src={`https://image.tmdb.org/t/p/original${detail?.poster_path}`}
+        alt={detail?.title}
+      />
+
+      <p className={styles.overview}>{detail?.overview}</p>
+      <p>release date : {detail?.release_date}</p>
+    </div>
+  );
+};
 export default MovieDetalis;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,46 +1,45 @@
-import React,{useState, useEffect} from "react";
-import styles from '../styles/home.module.css'
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import styles from "../styles/home.module.css";
 import { Link } from "react-router-dom";
 
 const url = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`;
 
-function Home () {
-    const [movies,setMovies] = useState([]);
-    const navigate = useNavigate();
-    const [id,setId] = useState();
+function Home() {
+  const [movies, setMovies] = useState([]);
 
-    const onClick = () => {
-        
+  useEffect(() => {
+    async function movieas() {
+      const response = await fetch(url);
+      const movieas = await response.json();
+
+      setMovies(movieas.results);
     }
-    
-    useEffect(()=>{
-        async function movieas () {
-            const response = await fetch(url);
-            const movieas = await response.json();
 
-            setMovies(movieas.results);
-        }
-        
     movieas();
-        
-    },[]);
-    
-    console.log(movies);
+  }, []);
 
-    return (
-        <div className={styles.movie_list}>
-            {movies && movies.length > 0 && movies.map((movie)=>
-                <div key={movie.id}  onClick={onClick}>
-                    <Link  to={`/${movie.id}`}>
-                        <img title={movie.title} id={movie.id} src={movie.backdrop_path && `https://image.tmdb.org/t/p/original${movie.backdrop_path}`} alt="mo"/>
-                        <p>{movie.title}</p>
-                    </Link>
-                </div>
-            )}
-            
-        </div>
-    )
+  return (
+    <div className={styles.movie_list}>
+      {movies &&
+        movies.length > 0 &&
+        movies.map((movie) => (
+          <div key={movie.id}>
+            <Link to={`/${movie.id}`}>
+              <img
+                title={movie.title}
+                id={movie.id}
+                src={
+                  movie.backdrop_path &&
+                  `https://image.tmdb.org/t/p/original${movie.backdrop_path}`
+                }
+                alt="mo"
+              />
+              <p>{movie.title}</p>
+            </Link>
+          </div>
+        ))}
+    </div>
+  );
 }
 
 export default Home;
